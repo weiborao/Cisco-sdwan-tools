092d0c21ee283eaee69975d324022e8029d3673f
weiborao <raoweibo@gmail.com>
2019-11-10 00:08:27 +0800

    Create sdwan_env_sample.py
    please change the name to sdwan_env.py to run.

---
+++ sdwan_env_sample.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+server_list = [
+    {
+        "server_name": "chusdwan",
+        "hostname": "180.37.12.38",
+        "port": 443,
+        "username": "admin",
+        "password": "xd13ddcdd",
+        "tenant": [
+            {
+                "name": "T1",
+                "id": "1553514871443",
+            },
+            {
+                "name": "T2",
+                "id": "1553514659609",
+            },
+            {
+                "name": "T3",
+                "id": "1559543230903",
+            }]
+    },
+    {
+        "server_name": "dcloudrtp",
+        "hostname": "dcloud-sdwan-inst-rtp.cisco.com",
+        "port": 443,
+        "username": "admin",
+        "password": "sxddwedd",
+        "tenant": "single_tenant_mode"
+    }
+]
